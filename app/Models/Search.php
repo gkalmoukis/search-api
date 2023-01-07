63a4b18f219bc44692f960b453a5e7d3fcfb0d7e
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-01-07 20:48:13 +0200

    Add search relatioships

--- app/Models/Search.php
+++ app/Models/Search.php
@@ -10,4 +10,14 @@ class Search extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function locations()
+    {
+        return $this->belongsToMany(Location::class);
+    }
+
+    public function types()
+    {
+        return $this->belongsToMany(Location::class);
+    }
 }
