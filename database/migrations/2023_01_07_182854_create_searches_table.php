84a500f2309f8721f12b5ec7e8299d2c04df5452
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-01-07 20:46:16 +0200

    Add search db schema

---
+++ database/migrations/2023_01_07_182854_create_searches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('searches', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip');
+            $table->unsignedInteger('price_min')->nullable();
+            $table->unsignedInteger('price_max')->nullable();
+            $table->unsignedInteger('square_meters_min')->nullable();
+            $table->unsignedInteger('square_meters_max')->nullable();
+            $table->string('availability')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('searches');
+    }
+};
