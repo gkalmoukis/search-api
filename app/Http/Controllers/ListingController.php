448410934d528460b5df54e609961fe9b15f4381
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-01-07 16:37:21 +0200

    Expose index route

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,6 +10,27 @@ class ListingController extends APIController
     public function __construct(
         protected ListingRepository $listings
     ) {}
+/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->success($this->listings->getAllListings());
+    }
 
+    // add doc
+    public function show($id)
+    {
+        try {
+            return $this->success($this->listings->getListingsById($id));
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return $this->fail($e->getMessage());
+        }
+        catch (\Exception $e) {
+            return $this->fail($e->getMessage());
+        }
+    }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    "prefix" => "listings" 
+], function (){
+    Route::get('/', [\App\Http\Controllers\ListingController::class, 'index']);
 });
+
