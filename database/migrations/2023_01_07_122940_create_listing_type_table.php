9d514627855f23192cf5cc765ff31e9503d13070
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-01-07 14:44:17 +0200

    Prototype db schema

---
+++ database/migrations/2023_01_07_122940_create_listing_type_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listing_type', function (Blueprint $table) {
+            $table->foreignId('listing_id')
+                ->constrained('listings')
+                ->onDelete('cascade');
+            $table->foreignId('type_id')
+                ->constrained('types')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+};
