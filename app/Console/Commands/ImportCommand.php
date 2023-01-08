28673a6d03bb98e600c646e8642766ef0ee7c492
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-01-08 14:34:56 +0200

    Init import command

---
+++ app/Console/Commands/ImportCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Imports\{
+    LocationImport
+};
+
+class ImportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        (new LocationImport)->withOutput($this->output)->import(storage_path()."/app/data/"."listings.xlsx");
+        return Command::SUCCESS;
+    }
+}
